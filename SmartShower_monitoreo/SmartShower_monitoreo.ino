d248db89f6280311020dcbec208e9b46a41847e5
Juan Jose Millan <juanjose@wenuwork.cl>
2018-08-10 14:57:13 -0400

    no message

--- SmartShower_monitoreo/SmartShower_monitoreo.ino
+++ SmartShower_monitoreo/SmartShower_monitoreo.ino
@@ -11,8 +11,8 @@
 //Definitions
 #define FLUJO_PIN           2
 #define TEMP_PIN            3
-#define drawTime            45000 //tiempo [ms] entre duchas
-#define calibrationFactor   8
+#define drawTime            5000 //tiempo [ms] entre duchas
+#define calibrationFactor   7
 
 //hardware
 TFT_ILI9341 tft = TFT_ILI9341();
@@ -25,7 +25,7 @@ bool                    flag_erase          = false;
 volatile byte           pulseCount;  
 float                   flowRate;
 unsigned long           flowMilliLitres;
-unsigned long           totalMilliLitres;
+
 unsigned long           temperature;
 unsigned long           oldTime;
 unsigned long           currentTime;
@@ -40,6 +40,8 @@ void pulseCounter(){
   pulseCount++;
 }
 //funcion timer que guarda el total de agua por un tiempo t y lo borra despues de ese tiempo
+
+/*
 void saveTotal(){
   if( totalMilliLitres - totalAgua > 100 ){
     totalAgua = totalMilliLitres;
@@ -48,19 +50,10 @@ void saveTotal(){
     totalMilliLitres = 0;
   }
 }
-//funcion para generar el mensaje con las variables
-String generateMessage(){
-  root = doc.to<JsonObject>();
-  root["f"] = String(flowMilliLitres);
-  root["t"] = String(totalMilliLitres);
-  root["T"] = String(temperature);
-  String dato;
-  serializeJson(root,dato);
-  return dato;
-}
+*/
 
-String generateMessage2(){
-  String dato = String(flowMilliLitres)+":"+String(totalMilliLitres)+":"+String(temperature);
+String generateMessage(){
+  String dato = String(flowMilliLitres)+":"+String(temperature);
   return dato;
 }
 
@@ -81,10 +74,6 @@ void printMedidas(){
   Serial.print("mL/Sec"); 
 
   // Print the cumulative total of litres flowed since starting
-  Serial.print("  Output Liquid Quantity: ");             // Output separator
-  Serial.print(totalMilliLitres);
-  Serial.print("mL");
-
   Serial.print("  Water temperature: ");             // Output separator
   Serial.print(temperature);
   Serial.println("*C");
@@ -104,29 +93,27 @@ void setup() {
   radio.setPALevel(RF24_PA_MIN);
   radio.stopListening();
   //Timer para saveTotal
-  MsTimer2::set(drawTime, saveTotal); // 10000ms period
-  MsTimer2::start();
+  //MsTimer2::set(drawTime, saveTotal); // 10000ms period
+  //MsTimer2::start();
   //Inicializacion de variables
   pulseCount        = 0;
   flowRate          = 0.0;
   flowMilliLitres   = 0;
-  totalMilliLitres  = 0;
   oldTime           = millis();   
-
+  
   attachInterrupt(digitalPinToInterrupt(FLUJO_PIN), pulseCounter, RISING); 
 }
 
 void loop(){
   currentTime = millis();
-  if((currentTime - oldTime) > 500)    // Only process counters once per second
+  if((currentTime - oldTime) > 450)    // Only process counters once per second
   { 
     detachInterrupt(FLUJO_PIN);
     flowRate = ((1000.0 / (currentTime - oldTime)) * pulseCount) / calibrationFactor;
     flowMilliLitres = (flowRate / 60) * 1000;
-    totalMilliLitres += flowMilliLitres;
     temperature = mlx.readObjectTempC();
     //printMedidas();
-    sendMessage(generateMessage2());
+    sendMessage(generateMessage());
     // Reset the pulse counter so we can start incrementing again
     pulseCount = 0;
 
